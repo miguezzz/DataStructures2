cdca26e653288b5223a66a505fb2d330e6050746
Victor <v.miguez2004@gmail.com>
2024-08-25 16:55:38 -0300

    modificação na main

--- mainag.c
+++ mainag.c
@@ -4,19 +4,82 @@
 #include "agencia.h"
 
 int main(int argc, char** argv) {
-    //declara ponteiro para arquivo
-    FILE *out;
-    //abre arquivo
-    if ((out = fopen("agencia.dat", "w+b")) == NULL) {
-        printf("Erro ao abrir arquivo\n");
-        exit(1);
+    
+    // else {
+    //     Agencia *ag = nova_ag(696969, "Agencia Vmigu", "Tangas");
+    //     salva_ag(ag, out);
+    //     imprime_ag(ag);
+    //     free(ag);
+    //     //fecha arquivo
+    //     fclose(out);    
+    // }
+    int continuar = 1;
+    do {
+        printf("O que deseja fazer?\n");
+
+        int op;
+        printf("\n1 - Cadastrar agência |\n2 - Ler Agência |\n3 - Mostrar todas as agências |\n4 - Excluir Agência |\n0 - Sair |\n");
+        scanf("%d", &op);
+        if (op == 0) {continuar = 0;}
+        switch (op) {
+            case 1:
+                //declara ponteiro para arquivo
+                FILE *out;
+                // tenta abrir/criaar o arquivo e encerra se nao funcionar
+                if ((out = fopen("agencias.dat", "w+b")) == NULL) {
+                printf("Erro ao abrir arquivo\n");
+                exit(1);
+                }
+
+                char continuar_adicionando = 'n';
+                do {
+                    printf("\nDigite o código da agência: ");
+                int cod;
+                scanf("%d", &cod);
+                // limpeza de buffer
+                getchar();                
+                
+                printf("Digite o nome da agência: ");
+                char nome_ag[50];
+                fgets(nome_ag, sizeof(nome_ag), stdin);
+                nome_ag[strcspn(nome_ag, "\n")] = '\0';  // Remove o caractere de nova linha
+
+                printf("Digite o nome do gerente: ");
+                char gerente[50];
+                fgets(gerente, sizeof(gerente), stdin);
+                nome_ag[strcspn(nome_ag, "\n")] = '\0';  // Remove o caractere de nova linha
+
+                Agencia *ag = (Agencia *) malloc(sizeof(Agencia));
+                ag = nova_ag(cod, nome_ag, gerente);
+                salva_ag(ag, out);
+                printf("Agência criada com sucesso: \n");
+                imprime_ag(ag);
+
+                free(ag);
+
+                printf("\n\nDeseja adicionar mais uma agência? [Y/n]");
+                } while (continuar_adicionando != 'Y');
+                
+                fclose(out);
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+            printf("Listagem de todas as agências:\n");
+
+                break;
+
+            case 4:
+
+                break;
+      
+        }
     }
-    else {
-        Agencia *ag = nova_ag(696969, "Agencia Vmigu", "Tangas");
-        salva_ag(ag, out);
-        imprime_ag(ag);
-        free(ag);
-        //fecha arquivo
-        fclose(out);    
-    }
+    while (continuar != 0);
+
+    printf("Encerrando...");
+    return 0;
 }
