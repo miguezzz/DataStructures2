6ef614db40b55b740f694d880db43c175a67a8d2
Victor Miguez <v.miguez2004@gmail.com>
2024-11-17 14:48:12 -0300

    left

--- leftkowitz/clientes.c
+++ leftkowitz/clientes.c
@@ -1,3 +1,6 @@
+#ifndef CLIENTES_H
+#define CLIENTES_H
+
 #include "clientes.h"
 
 // Cria uma conta corrente. Lembrar de usar free();
@@ -7,7 +10,7 @@ Cliente *criaCliente(int cod_cliente, char *nome, int idade) {
 
     c->cod_cliente = cod_cliente;
     strcpy(c->nome, nome);
-    strcpy(c->idade, idade);
+    c->idade = idade;
 
     return c;
 }
@@ -15,11 +18,11 @@ Cliente *criaCliente(int cod_cliente, char *nome, int idade) {
 // Salva um cliente num arquivo externo
 void salvaCliente(Cliente *c, FILE *out) {
     if (fwrite(&c->cod_cliente, sizeof(int), 1, out) < 1) {
-        free(c);
+        // free(c);
         exit(1);
     }
     fwrite(c->nome, sizeof(char), 50, out);
-    fwrite(c->idade, sizeof(int), 1, out);
+    fwrite(&c->idade, sizeof(int), 1, out);
 }
 
 // Função para carregar clientes em memória
@@ -27,40 +30,59 @@ Cliente *carregaCliente(FILE *in) {
 
     Cliente *c = (Cliente *) malloc(sizeof(Cliente));
     if (c == NULL) {
-        free(c);
         return NULL; // falha ao puxar cliente (fim do arquivo)
     }
 
     if (fread(&c->cod_cliente, sizeof(int), 1, in) != 1) {
-        // printf("\nFim do arquivo...\n");
+        printf("\nFim do arquivo...\n");
         free(c);
         return NULL; // Falha na leitura do cod_cliente
     }
     //printf("-------%d", sizeof(char) * sizeof(c->nome));
-    fread(c->nome, sizeof(char), sizeof(c->nome), in);
-    fread(c->idade, sizeof(int), sizeof(c->idade), in);
+    fread(c->nome, sizeof(char), 50, in);
+    fread(&c->idade, sizeof(int), 1, in);
+
+    printf("Cliente carregado: %s\n", c->nome);
+    
     return c;
 }
 
 void leClientes(FILE *in) {
     printf("\n\nLendo clientes do arquivo...\n\n");
-    //rewind(in);
+
+    rewind(in);
+
     int contador = 0;
     Cliente *c;
+
     while ((c = carregaCliente(in)) != NULL) {
         printf("%d\n", ++contador);
         imprimeCliente(c);
-        free(c);
+        //free(c);
     }
 }
 
-void leftkowitz() {
-    //função 1
-    // passo 1: cria arquivo 2 e chama A2() passando o A1 e o novo A2
+// Função para imprimir os dados do cliente
+void imprimeCliente(Cliente *c) {
+    
+    if (c == NULL) {
+        return;
+    }
 
-    //função 2
-    // passo 2: cria arquivo 3 e chama A3() passando o A2 e o novo A3_idade
-    // passo 3: ordena o arquivo 3 por idade gerando o A4
+    printf("**********************************************");
+    printf("\nCódigo do cliente: %d", c->cod_cliente);
+    printf("\nNome do cliente: %s", c->nome);
+    printf("\nIdade: %d", c->idade);
+    printf("\n**********************************************\n\n");
+}
+
+void leftkowitz(FILE *in) {
+    
+    A2(in);
+
+    A3(in);
+
+    A4(in);
 
     //função 3
     // gerar A5 e A6
@@ -68,7 +90,101 @@ void leftkowitz() {
     // juntar A1 com A7 e gerar A8
 }
 
-void A2() {
-    
+void A2(FILE *in) {
+
+    //criando A2
+    const char *A2 = "A2.dat";
+
+    FILE *out = fopen(A2, "w+b");
+    if (!out) {
+        printf("Erro ao criar o arquivo A2.\n");
+        exit(1);
+    }
+
+    int ED = 1; // ED = endereço de disco
+    Cliente *c;
+    rewind(in);
+    while ((c = carregaCliente(in)) != NULL) {
+        if (fwrite(&ED, sizeof(int), 1, out) != 1) {
+            printf("Erro ao escrever ED no arquivo A2.\n");
+            free(c);
+            break;
+        }
+        printf("ED: %d\n", ED);
+        fwrite(&c->cod_cliente, sizeof(int), 1, out);
+        fwrite(&c->idade, sizeof(int), 1, out);
+
+        ED++;
+    }
 }
 
+void A3(FILE *in) {
+
+    //criando A2
+    const char *A3 = "A3.dat";
+
+    FILE *out = fopen(A3, "w+b");
+    if (!out) {
+        printf("Erro ao criar o arquivo A2.\n");
+        exit(1);
+    }
+
+    int ED = 1; // ED = endereço de disco
+    Cliente *c;
+    rewind(in);
+    while ((c = carregaCliente(in)) != NULL) {
+        if (fwrite(&ED, sizeof(int), 1, out) != 1) {
+            printf("Erro ao escrever ED no arquivo A2.\n");
+            free(c);
+            break;
+        }
+        printf("ED: %d\n", ED);
+        fwrite(&c->cod_cliente, sizeof(int), 1, out);
+        fwrite(&c->idade, sizeof(int), 1, out);
+
+        ED++;
+    }
+}
+
+void A4(FILE *in) {
+    const char *A4 = "A4.dat";
+
+    FILE *out = fopen(A4, "w+b");
+
+    if (!out) {
+        printf("Erro ao criar o arquivo A4.\n");
+        exit(1);
+    }
+
+    int numClientes = 0;
+    Cliente *clientes[100]; // Assuming a maximum of 100 clients
+
+    rewind(in);
+    Cliente *c;
+    while ((c = carregaCliente(in)) != NULL) {
+        clientes[numClientes++] = c;
+    }
+
+    qsort(clientes, numClientes, sizeof(Cliente *), comp);
+
+    for (int i = 0; i < numClientes; i++) {
+        fwrite(&clientes[i]->cod_cliente, sizeof(int), 1, out);
+        fwrite(clientes[i]->nome, sizeof(char), 50, out);
+        fwrite(&clientes[i]->idade, sizeof(int), 1, out);
+        free(clientes[i]);
+    }
+
+    imprimeCliente(c);
+
+    fclose(out);
+}
+
+// comparador pra idade (vai ser passado para a função qsort no A4)
+int comp(const void* a, const void* b) {
+    Cliente *clienteA = *(Cliente **)a;
+    Cliente *clienteB = *(Cliente **)b;
+
+    return (clienteA->idade - clienteB->idade);
+}
+
+#endif
---
+++ leftkowitz/main.c
@@ -0,0 +1,59 @@
+#include "clientes.h"
+
+int main(void) {
+    Cliente *joao = criaCliente(1, "Joao", 21);
+    Cliente *maria = criaCliente(2, "Maria", 19);
+    Cliente *jose = criaCliente(3, "Jose", 21);
+    Cliente *ana = criaCliente(4, "Ana", 77);
+    Cliente *pedro = criaCliente(5, "Pedro", 55);
+    Cliente *paulo = criaCliente(6, "Paulo", 24);
+    Cliente *lucas = criaCliente(7, "Lucas", 21);
+    Cliente *marcos = criaCliente(8, "Marcos", 19);
+    Cliente *lucia = criaCliente(9, "Lucia", 24);
+    Cliente *marta = criaCliente(10, "Marta", 77);
+
+    FILE *clientes = fopen("A1.dat", "w+b");
+
+    salvaCliente(joao, clientes);
+    free(joao);
+
+    salvaCliente(maria, clientes);
+    free(maria);
+
+    salvaCliente(jose, clientes);
+    free(jose);
+
+    salvaCliente(ana, clientes);
+    free(ana);
+
+    salvaCliente(pedro, clientes);
+    free(pedro);
+
+    salvaCliente(paulo, clientes);
+    free(paulo);
+
+    salvaCliente(lucas, clientes);
+    free(lucas);
+
+    salvaCliente(marcos, clientes);
+    free(marcos);
+
+    salvaCliente(lucia, clientes);
+    free(lucia);
+
+    salvaCliente(marta, clientes);
+    free(marta);
+
+    fclose(clientes);
+
+    FILE *A1 = fopen("A1.dat", "rb");
+    rewind(A1);
+
+    leftkowitz(A1);
+
+    printf("Fim do programa.\n");
+
+    fclose(A1);
+    
+    return 0;
+}
